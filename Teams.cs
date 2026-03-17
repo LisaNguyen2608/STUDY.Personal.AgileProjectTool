741b17c51ca9f747b4e718d1e4ce9df06a50b0dd
penttia <pentti.avolahti@student.lab.fi>
2026-03-17 12:59:33 +0200

    Update Teams.cs

--- Teams.cs
+++ Teams.cs
@@ -1,25 +1,83 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.14.37012.4 d17.14
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "Teams", "Teams\Teams.csproj", "{98C7C4AB-810A-4F13-9C6E-CF7F7F8C6F5F}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|Any CPU = Debug|Any CPU
-		Release|Any CPU = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{98C7C4AB-810A-4F13-9C6E-CF7F7F8C6F5F}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{98C7C4AB-810A-4F13-9C6E-CF7F7F8C6F5F}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{98C7C4AB-810A-4F13-9C6E-CF7F7F8C6F5F}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{98C7C4AB-810A-4F13-9C6E-CF7F7F8C6F5F}.Release|Any CPU.Build.0 = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {49259A02-1F3E-408B-9515-65619DDAFAFC}
-	EndGlobalSection
-EndGlobal
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+		//these Person Classes are personal notes
+  /*      public class Person 
+        {
+            private int personID;
+            private string name;
+            private string role;
+
+
+            public Person(int person_ID, string nm, string personRole)
+            {
+                personID = person_ID;
+                name = nm;
+                role = personRole;
+
+            }
+            public override string ToString()
+            {
+                return personID + ": " + name + ", role: " + role + "\n";
+            }
+        }
+       */
+        public class Team 
+        {
+            private int teamID;
+          //  private int personID;
+            private Person[] persons; //person array
+            const int MAX_PERSONS = 100; 
+
+
+
+            public Team(int team_ID) //constructor with teamID and persons
+            {
+                teamID = team_ID;
+                persons = new Person[MAX_PERSONS];
+            }
+            public void SetTeamID(int team_ID)
+            {
+                teamID = team_ID;
+            }
+            public bool AddPerson(Person person)
+            {
+                int i = 0;
+                while (i < MAX_PERSONS && persons[i] != null)
+                    i++;
+                if (i <  MAX_PERSONS)
+                {
+                    persons[i] = person;
+                    return true;
+                }
+                return false;
+            }
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder("Team's ID: " + teamID + "\n ");
+                for (int i = 0; i < MAX_PERSONS; i++)
+                {
+                    if (persons[i] != null)
+                    {
+                        sb.AppendLine(persons[i].ToString());
+                    }
+                }
+                return sb.ToString();
+            }
+            
+
+        }
+    }
+}
